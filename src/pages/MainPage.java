e528cd8abbf47817e66e025603b908dc932f77bb
G4briell3 <Gabriela.Diana.Surdu@gmail.com>
2025-01-28 22:08:00 +0200

    MainPage stated

--- src/pages/MainPage.java
+++ src/pages/MainPage.java
@@ -1,5 +1,19 @@
 package pages;
 
-public class MainPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
+public class MainPage 
+{
+  WebDriver driver;
+  
+  public  MainPage(WebDriver driver)
+  {
+	  this.driver=driver;
+  }
+  
+  private By pageHeader=By.xpath("//div[@class=\"pb-html\"]/h2[contains(.,\"Bun venit pe Dr. Max\")]");
+  private By loginButton=By.xpath("//button[@id=\"login-btn-header\"]");
+  private By loginButtonFromModal=By.xpath("//button[@class=\"mb-3 drmax-btn drmax-btn--primary drmax-btn--medium drmax-btn-icon--left\"]");
+  
 }
