c290da51aa58cb2a8b17381b9fb4170b97e93d45
G4briell3 <Gabriela.Diana.Surdu@gmail.com>
2025-01-29 21:45:17 +0200

    added some elements from the register page

--- src/pages/MainPage.java
+++ src/pages/MainPage.java
@@ -15,5 +15,7 @@ public class MainPage
   private By pageHeader=By.xpath("//div[@class=\"pb-html\"]/h2[contains(.,\"Bun venit pe Dr. Max\")]");
   private By loginButton=By.xpath("//button[@id=\"login-btn-header\"]");
   private By loginButtonFromModal=By.xpath("//button[@class=\"mb-3 drmax-btn drmax-btn--primary drmax-btn--medium drmax-btn-icon--left\"]");
-  
-}
+  private By createAccountButton=By.xpath("//button[@id=\"login-register-btn\"]");
+  private By emailField=By.xpath("//input[@id=\"username\"]");
+  private By passwordField=By.xpath("//input[@id=\"current-password\"]");
+  }
---
+++ src/pages/RegisterUserPage.java
@@ -0,0 +1,25 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class RegisterUserPage 
+{
+   WebDriver driver;
+   
+   public RegisterUserPage( WebDriver driver)
+   {
+	   this.driver=driver;
+   }
+   
+   private By existingLoialityCardButton=By.xpath("//h2[@class=\"h-title-medium radio-title\"][contains(.,\"Am deja un card\")]");
+   private By newLoialityCardButton=By.xpath("//h2[@class=\"h-title-medium radio-title\"][contains(.,\"Nu am un card\")]");
+   private By emailField=By.id("email");
+   private By continueButton=By.xpath("//button[@class=\"w-100 drmax-btn drmax-btn--primary drmax-btn--medium drmax-btn-icon--left\"]");
+   private By firstNameField=By.id("firstName");
+   private By lastNameField=By.id("lastName");
+   private By passwordField=By.id("password");
+   private By termsAndConditionsAgreementButton=By.xpath("//div[@class=\"drmax-selection__text w-100\"]/div/span/a/following-sibling::a[@href=\"https://www.drmax.ro/politica-confidentialitate\"]");
+   private By advertisingAgreementButton=By.xpath("//div[@class=\"drmax-selection__text w-100\"]/div/span[contains(.,\"Sunt de acord sa primesc \")]");
+   private By submitButton=By.xpath("//button[@class=\"w-100 mt-2 drmax-btn drmax-btn--primary drmax-btn--medium drmax-btn-icon--left\"]/div/span[contains(.,\"Finalizeaza inregistrarea\")]");
+}
